101a6f025a89138fac7d0f75f82aef3b75c5c5eb
Neeraj Kumar <neerajkr600@gmail.com>
2024-12-10 13:05:05 +0530

    adding comment to student

--- src/main/java/com/example/rest_api/Students.java
+++ src/main/java/com/example/rest_api/Students.java
@@ -5,6 +5,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+//Entity
 @Document(collection = "students")
 //@Data
 public class Students {
