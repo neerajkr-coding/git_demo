4afde551df90694300278102df4167475f82a81f
Neeraj Kumar <neerajkr600@gmail.com>
2024-12-10 13:09:46 +0530

    Comment to the MyController

--- src/main/java/com/example/rest_api/MyController.java
+++ src/main/java/com/example/rest_api/MyController.java
@@ -24,6 +24,7 @@ public class MyController {
         return studentService.getAllStudent();
     }
 
+    //to post student
     @PostMapping("/post")
     public Students createUser(@RequestBody Students students) {
         return studentService.createStudent(students);
