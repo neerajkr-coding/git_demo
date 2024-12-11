3b37454aa1d306cd1a9686dc8976db3791b5da7c
Neeraj Kumar <134834249+neerajkr-coding@users.noreply.github.com>
2024-12-11 12:16:55 +0530

    Update temp.java: Added comment

--- src/main/java/com/example/rest_api/temp.java
+++ src/main/java/com/example/rest_api/temp.java
@@ -1,9 +1,11 @@
 package com.example.rest_api;
 
+//This file is a dummy file for my git practice
 public class temp {
 
     public static void main(){
         System.out.print("Hello world");
+        
     }
 
 }
